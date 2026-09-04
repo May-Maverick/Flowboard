46b872280be272c122b48b854d944e00d830c642
Ntokozo May <mayntokozo19027@gmail.com>
2026-09-04 21:15:40 +0200

    feat: Add userModel functions addUser + getUser

---
+++ backend/db/models/userModel.js
@@ -0,0 +1,13 @@
+import pool from "../../config/database.js";
+
+export const addUser = async (email, passwordHash, firstName, lastName) => {
+    const query = "INSERT INTO user(email, password_hash, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING *";
+    const response = await pool.query(query, [email, passwordHash, firstName, lastName]);
+    return response.rows[0];
+};
+
+export const getUser = async (id) => {
+    const query = "SELECT * FROM users WHERE id = $1";
+    const response = await pool.query(query, [id]);
+    return response.rows[0];
+};
